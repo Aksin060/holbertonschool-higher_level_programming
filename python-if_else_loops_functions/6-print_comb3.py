15837ef7b7aaeaaf5d238cdf0420b35c1dd85108
Aksin <11200@holbertonstudents.com>
2025-06-16 09:47:49 +0000

    Salam

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range (10):
-    for j in range (i+1 , 10):
-        if i!=8 or j!=9:
-            print("{:02},{:02}".format(i, j), end=", ")
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:01}{:01}".format(i, j), end=", ")
         else:
-            print("{:02},{:02}".format(i, j))
+            print("{:01}{:01}".format(i, j))
