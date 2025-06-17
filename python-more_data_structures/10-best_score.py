b2e09cf80ceeba22de09e20e845fae07b7d4bf42
Aksin <11200@holbertonstudents.com>
2025-06-17 16:25:46 +0000

    Salam

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if key in a_dictionary:
+        max = 0
+        for key, value in a_dictionary:
+            if a_dictionary[key] > max:
+                max = a_dictionary[key]
+    return max
