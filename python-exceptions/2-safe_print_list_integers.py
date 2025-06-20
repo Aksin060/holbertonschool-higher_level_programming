1fc431432a2f759f7ebab2335a955ad5dfb3478d
Aksin <11200@holbertonstudents.com>
2025-06-20 09:07:23 +0000

    Salam

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     i = 0
     try:
         while i < x:
-            if type(my_list[i]) is int:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
             i += 1
