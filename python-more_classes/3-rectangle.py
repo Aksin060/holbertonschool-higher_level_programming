6690cd84b489be0a09e779370630a6256371c856
Aksin <11200@holbertonstudents.com>
2025-06-26 08:43:37 +0000

    Salam

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,5 +47,5 @@ class Rectangle:
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            return""
+            return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
